e1566380f605ed29ac04b027af19e2b69b36f474
ibesoft11 <36492204+ibesoft11@users.noreply.github.com>
2018-05-22 20:23:23 +0100

    Add files via upload
    Added interface for our school objects

---
+++ schoolRecord.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    //My interface, shows the structure of my program
+    interface schoolRecord
+    {
+        void addNew(string studentName);
+        void getStudents();
+        void deleteStudent(string studentName);
+        void viewSchool();
+    }
+}
