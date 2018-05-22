15777706848b4a180032b6f09f359a090b548d0e
ibesoft11 <36492204+ibesoft11@users.noreply.github.com>
2018-05-22 20:24:10 +0100

    Add files via upload
    Added abstract class to implement our schoolRecord interface

---
+++ schoolStructure.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    //Abstract class
+    public abstract class schoolStructure : schoolRecord
+    {
+        public void addNew(string studentName) { }
+        public void addNew(string studentName, string level) { }
+        public void getStudents(){}
+        public void deleteStudent(string studentName) { }
+
+        //implement viewSchool of interface
+        public void viewSchool()
+        {
+            Console.WriteLine("Ibesoft Comprehensive School");
+        }
+    }
+}
