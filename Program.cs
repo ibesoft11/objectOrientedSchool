f77c0131eb26ebf484dd07768bddfcea7650b16c
ibesoft11 <36492204+ibesoft11@users.noreply.github.com>
2018-05-22 20:28:20 +0100

    Add files via upload
    Added main class to call and use our already created school objects

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Dependency injection occurs here, I called the addNew without having to re-write it here
+            */
+            School school = new School();
+            school.viewSchool();
+            school.addNew("ibesoft");
+            school.addNew("kenny gee", "500");
+            school.addNew("val");
+            school.getStudents();
+            Console.ReadKey();
+        }
+    }
+}
