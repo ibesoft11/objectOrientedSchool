db07f2732f0a95926ee97b5bd3b9c7463b7f4bb8
ibesoft11 <36492204+ibesoft11@users.noreply.github.com>
2018-05-22 20:24:53 +0100

    Add files via upload
    Added the main School class to manage operations of the abstract class

---
+++ School.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    class School : schoolStructure
+    {
+        int studCount = 0;
+        public void addNew(string studentName)
+        {
+            studCount++;
+            Console.WriteLine("New student added, Name : {0}", studentName);
+        }
+        public void addNew(string studentName, string level) {
+            studCount++;
+            Console.WriteLine("New student added, Name : {0},  Level : {1}", studentName, level);
+        }
+        public void getStudents()
+        {
+            Console.WriteLine(studCount);
+        }
+        public void deleteStudent(string studentName)
+        {
+            studCount--;
+            Console.WriteLine("Name : {0}, has been deleted", studentName);
+        }
+    }
+}
